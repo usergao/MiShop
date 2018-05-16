971e5deaaf3bb88506cbc41502d2d1ff79ca0024
高明 <gaoming@gaomingdeMBP.local>
2018-05-16 11:51:24 +0800

    none

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -9,7 +9,7 @@ class AuthController extends Controller
 {
     public function mobileOauth(Request $request)
     {
-        return \Socialite::with('weixin')->setScopes(['snsapi_base'])->redirect();
+        return \Socialite::with('weixin')->redirect();
     }
 
     public function mobileCallback(Request $request)
