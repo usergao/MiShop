8aac7d93a7384347d2a60bef48065793b2d690b3
高明 <gaoming@gaomingdeMBP.local>
2018-05-16 11:06:35 +0800

    silence

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -46,6 +46,6 @@ class AuthController extends Controller
     // 静默授权登录
     public function silenceOauth(Request $request)
     {
-        return \Socialite::driver('weixin')->setScopes(['snsapi_base'])->with(['hd' => 'example.com'])->redirect();
+        return \Socialite::driver('weixin')->setScopes(['snsapi_base'])->redirect();
     }
 }
